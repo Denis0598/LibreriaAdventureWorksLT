cbc76b40e9a68db0d6ba1b210ae08ad3fa26f675
DENIS EDUARDO BANCES LUNA <dbancesl1@miumg.edu.gt>
2022-07-19 22:22:44 -0600

    Carga inicial

---
+++ LibreraiAdventureWorks/LibreraiAdventureWorks/data/AdventureWorksContextExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorksNS.Data
+{
+    public static class AdventureWorksContextExtension
+    {
+        public static IServiceCollection AdventureWorksDBContext(
+            this IServiceCollection services, 
+            string strCnx = "Data Source=DESKTOP-K0P85KI;Initial Catalog=AdventureWorksLT2017;Integrated Security=false;User=usuario1;Password=1234;")
+        {
+            services.AddDbContext<AdventureWorksDB>(options => options.UseSqlServer(strCnx));
+            return services;
+        }
+    }
+}
